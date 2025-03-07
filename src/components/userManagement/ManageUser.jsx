c7766e0b96e8976bde18036ca2a36172cfa72747
Darshan Bhensdadia <darshan.bhensdadia@rapdiops.co>
2025-03-07 12:14:54 +0530

    Custom roles added

--- src/components/userManagement/ManageUser.jsx
+++ src/components/userManagement/ManageUser.jsx
@@ -174,11 +174,11 @@ const ManageUser = () => {
                         <table className="w-full text-sm text-left text-gray-500">
                             <thead className="text-xs text-gray-700 uppercase bg-gray-50">
                                 <tr>
-                                    <th className="px-6 py-3">Name</th>
-                                    <th className="px-6 py-3">Email</th>
-                                    <th className="px-6 py-3">Role</th>
-                                    <th className="px-6 py-3">Teams</th>
-                                    <th className="px-6 py-3">Actions</th>
+                                    <th className="px-2 py-3">Name</th>
+                                    <th className="px-2 py-3">Email</th>
+                                    <th className="px-2 py-3">Role</th>
+                                    <th className="px-2 py-3">Teams</th>
+                                    <th className="px-2 py-3">Actions</th>
                                 </tr>
                             </thead>
                             <tbody>
